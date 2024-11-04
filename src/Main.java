ff72cdc6f0fe50dccc064bccbf311a10dc2359b6
FilipeEust <filiperafael007@gmail.com>
2024-11-04 17:28:08 -0300

    Update Full Tabuada

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,12 @@
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
 
-        System.out.print("Tabuada de: ");
-        int num = input.nextInt();
         for (int i = 0; i <= 10; i++) {
-            System.out.printf("%d x %d = %d\n", num, i, num*i);
+            System.out.printf("Tabuada de : %d\n",i);
+            for (int j = 0; j <= 10; j++) {
+                System.out.printf("%d * %d = %d\n", i, j, i * j);
+            }
+            System.out.println("\n");
         }
     }
 }
